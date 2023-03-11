8c6a05cb7e3ea514f8e4f43d105d1fb8bb471c77
Kittisak Kaewneam <kittisak84@gmail.com>
2023-03-11 10:55:02 +0700

    ลบไฟล์

--- inc/foot.php
+++
@@ -1,6 +0,0 @@
-    <!-- Link to Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    </body>
-
-    </html>
